1cfefa247c62412103b151e6c898d85cec6bb5fa
ctdoan33 <doanchristopher_1@yahoo.com>
2017-11-01 19:02:54 -0700

    based on local time of server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+	datetime = Date.now();
+	selected = null;
+	time = function(offset){
+		this.datetime = Date.now();
+		if(offset == "clear"){
+			this.selected = null;
+		}else{
+			this.datetime += offset*60*60*1000;
+			this.selected = offset;
+		}
+	}
 }
